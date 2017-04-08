bc95405557447405f2654a2a58283a67a42b8625
Nahid <nahidshaiket10300@gmail.com>
2017-04-08 16:52:46 +0600

    tweaking

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -25,13 +25,13 @@ export class Login extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="page-title">Todo App</h1>
+        <h1 className="page-title">Most Private Mesenger</h1>
         <div className="row">
           <div className="columns small-centered small-10 medium-6 large-4">
             <div className="callout callout-auth">
               <h3>Login</h3>
               <p>
-                Login with Faebook.
+                Login with Facebook.
               </p>
               <FacebookLogin
                 appId="342612186136243"

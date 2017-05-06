be52ba94e7d6afb135a4c63244b0fdc21ae021ea
Nahid <nahidshaiket10300@gmail.com>
2017-05-07 00:26:58 +0600

    pushing to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -32,16 +32,6 @@ module.exports = {
       compressor : {
         warnings: false
       }
-    }),
-    new webpack.DefinePlugin({
-      'process.env' : {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        API_KEY: JSON.stringify(process.env.API_KEY),
-        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
-        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
-        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
-        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
-      }
     })
   ],
 

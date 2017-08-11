4ba7b6054b393a4d9cce1e0b5fc4c769e93a5d0d
Nahid Shaiket <nahidshaiket10300@gmail.com>
2017-08-11 20:27:30 +0600

    Ignore

--- app/components/Messages.jsx
+++ app/components/Messages.jsx
@@ -17,27 +17,25 @@ export class Messages extends React.Component {
 
     componentDidMount() {
         const {socket} = this.props;
-        const messagesContainer = ReactDOM.findDOMNode(this.messagesContainer);
         socket.on('something else', function (msg) {
             var message = msg.message;
-            console.log(message);
             if (msg.id == (JSON.parse(localStorage.auth_user)._id)) {
-                console.log(JSON.stringify(msg));
                 $('.messages').append(
                     '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">' +
                     '<p class="my-message">' + message +
                     '</p></div>'
                 );
-                this.scrollToBottom();
+                if ($('div.messages').scrollTop + $('div.messages').clientHeight === $('div.messages').scrollHeight) {
+                    $('div.messages').scrollTop = $('div.messages').scrollHeight;
+                }
             } else {
-                console.log(JSON.stringify(msg));
                 $('.messages').append(
                     '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">' +
                     '<p class="friends-message">' + message +
                     '</p></div>'
                 );
-                this.scrollToBottom();
             }
+
         });
         this.scrollToBottom();
     }

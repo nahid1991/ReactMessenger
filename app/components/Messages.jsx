b561ca53cfe7d67ca5a5842cca55f89a39ab8af6
Nahid Shaiket <nahidshaiket10300@gmail.com>
2017-08-12 00:07:37 +0600

    Now when message added it scrolls down.

--- app/components/Messages.jsx
+++ app/components/Messages.jsx
@@ -25,15 +25,14 @@ export class Messages extends React.Component {
                     '<p class="my-message">' + message +
                     '</p></div>'
                 );
-                if ($('div.messages').scrollTop + $('div.messages').clientHeight === $('div.messages').scrollHeight) {
-                    $('div.messages').scrollTop = $('div.messages').scrollHeight;
-                }
+                $('.messages').scrollTop($('.messages').height()*10000);
             } else {
                 $('.messages').append(
                     '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">' +
                     '<p class="friends-message">' + message +
                     '</p></div>'
                 );
+                $('.messages').scrollTop($('.messages').height()*10000);
             }
 
         });

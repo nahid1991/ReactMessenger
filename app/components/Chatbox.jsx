2c8ab88b0b0ce19968b95246796d4fd3a063b2aa
Nahid Shaiket <nahidshaiket10300@gmail.com>
2017-08-13 15:20:53 +0600

    Added font awesome

--- app/components/Chatbox.jsx
+++ app/components/Chatbox.jsx
@@ -7,6 +7,24 @@ export class Chatbox extends React.Component {
         const {socket} = this.props;
         if (e.key == 'Enter') {
             e.preventDefault();
+            if(this.refs.message.value != ''){
+                var formattedData = {
+                    id: (JSON.parse(localStorage.auth_user))._id,
+                    message: this.refs.message.value
+                };
+
+                socket.emit('something else', formattedData);
+                this.refs.message.value = '';
+                this.refs.message.focus();
+            }
+
+        }
+    }
+
+    sendMessageButton(e) {
+        const {socket} = this.props;
+        e.preventDefault();
+        if(this.refs.message.value != ''){
             var formattedData = {
                 id: (JSON.parse(localStorage.auth_user))._id,
                 message: this.refs.message.value
@@ -18,29 +36,16 @@ export class Chatbox extends React.Component {
         }
     }
 
-    sendMessageButton(e) {
-        const {socket} = this.props;
-        e.preventDefault();
-        var formattedData = {
-            id: (JSON.parse(localStorage.auth_user))._id,
-            message: this.refs.message.value
-        };
-
-        socket.emit('something else', formattedData);
-        this.refs.message.value = '';
-        this.refs.message.focus();
-    }
-
     render() {
         return (
             <div>
                 <nav className="navbar navbar-default navbar-fixed-bottom center bottom-nav">
                     <div className="container-fluid navbar-inner">
                         <div className="input-group">
-              <textarea className="form-control custom-control custom" rows="3" ref="message"
-                        onKeyPress={this.sendMessage.bind(this)}></textarea>
+                            <textarea className="form-control custom-control custom" rows="2" cols="8" ref="message"
+                                onKeyPress={this.sendMessage.bind(this)}></textarea>
                             <span className="input-group-addon btn btn-default"
-                                  onClick={this.sendMessageButton.bind(this)}>Send</span>
+                                  onClick={this.sendMessageButton.bind(this)}><i className="fa fa-paper-plane"></i></span>
                         </div>
                     </div>
                 </nav>
